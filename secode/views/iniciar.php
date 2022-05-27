17719b82a6e34a496222a1c81148a91938741648
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2022-05-27 15:37:13 -0500

    carpetas mvc

--- secode/views/iniciar.php
+++ secode/views/iniciar.php
@@ -20,7 +20,7 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
 
   if (filter_var($email_user, FILTER_VALIDATE_EMAIL)) {
 
-    $consult = " SELECT Correo, Contrasena,Ndocumento FROM usuario WHERE Correo= :correo";
+          $consult = " SELECT Correo, Contrasena,Ndocumento FROM usuario WHERE Correo= :correo";
     $parametros = $connection->prepare($consult);
     $parametros->bindParam(':correo', $email_user);
     $parametros->execute();
