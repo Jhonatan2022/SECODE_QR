c7c7b1989e3cd7305c945ab509232e8300af25cc
Luis Fernando <luis.chaparro173@educacionbogota.edu.co>
2022-08-05 10:07:48 -0500

    avance rama qr

---
+++ secode/views/dashboard.php
@@ -0,0 +1,126 @@
+<?php
+if(isset($_SESSION[""]))
+
+
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php
+include('./templates/header.php');
+?>
+<body>
+
+	<!--PreLoader-->
+    <div class="loader">
+		<div class="inner"></div>
+		<div class="inner"></div>
+		<div class="inner"></div>
+		<div class="inner"></div>
+		<div class="inner"></div>
+	</div>
+    <!--PreLoader Ends--> 
+    <div class=container">
+
+        <header>
+            	<!-- header -->
+	<div class="top-header-area" id="sticker">
+		<div class="container bg-primary">
+			<div class="row">
+				<div class="col-lg-12 col-sm-12 text-center">
+					<div class="main-menu-wrap">
+
+						<!-- menu start -->
+						<nav class="main-menu ">
+							<ul>
+								<li class="current-list-item"><a href="index.html">Inicio</a></li>
+								<li><a href="nosotros.html">Quienes Somos</a></li>	
+								<li><a href="#">Noticias</a>
+									<ul class="sub-menu">
+										<li><a href="noticias.html">Noticias</a></li>
+										<li><a href="noticia.html">Noticia Del día</a></li>
+									</ul>
+								</li>
+								<li><a href="contact.html">Contáctanos</a></li>
+								<li><a href="#">Solicitar Código</a>
+								<ul class="sub-menu">
+									<li><a href="clinico.html">Datos Clinicos</a>
+									</li>
+								</ul>
+								<li class="login-box"><a href="#">
+									<span></span>
+									<span></span>
+									<span></span>
+									<span></span> SECODE_QR PLUS </a></li>
+							</ul>
+						</nav>	
+						<div class="mobile-menu"></div>
+						<!-- menu end -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end header -->
+        </header>
+
+        <main class="container">
+
+            
+<!-- product section -->
+<div class="product-section mt-150 mb-150 pt-4">
+		<div class="container">
+		<div class="container pt-4">
+                <h1>
+                    Mis codigos QR
+                </h1>
+            </div>
+			<div class="roww">
+				<div class="col-lg-4 col-md-6 text-center">
+					<div class="single-product-item">
+						<div class="product-image">
+							<a href="single-product.html"><img src="./assets/img/qrfor.png" alt=""></a>
+						</div>
+						<h3>name qr</h3>
+						<p class="product-price"><span>QR Para solicitar datos básicos <br> de la persona</span> </p>
+						<a href="cart.html" class="cart-btn"><i class="fas fa-pen"></i> opciones</a>
+					</div>
+				</div>
+			</div>
+
+            <div class="roww">
+				<div class="col-lg-4 col-md-6 text-center">
+					<div class="single-product-item">
+						<div class="product-image">
+							<a href="single-product.html"><img src="./assets/img/qrfor.png" alt=""></a>
+						</div>
+						<h3>name qr</h3>
+						<p class="product-price"><span>QR Para solicitar datos básicos <br> de la persona</span> </p>
+						<a href="cart.html" class="cart-btn"><i class="fas fa-pen"></i> opciones</a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<!-- end product section -->
+
+        </main>
+
+<?php
+
+
+?>
+        
+    </div>
+</body>
+
+</html>
---
+++ secode/views/templates/header.php
@@ -0,0 +1,25 @@
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- favicon -->
+	<link rel="shortcut icon" type="image/png" href="./assets/img/logo.png">
+    <title>Dashboard</title>
+	<!-- google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+	<!-- fontawesome -->
+	<link rel="stylesheet" href="assets/css/all.min.css">
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<!-- animate css -->
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<!-- mean menu css -->
+	<link rel="stylesheet" href="assets/css/meanmenu.min.css">
+	<!-- main style -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- responsive -->
+	<link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="./assets/css/dashborad.css">
+    
+</head>
