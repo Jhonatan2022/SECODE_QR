933fcb331a33171387e23efe332759f45ec2136e
Luis Fernando <luis.chaparro173@educacionbogota.edu.co>
2022-09-05 00:20:44 -0500

    fix sweetalerts2

--- secode/views/templates/sweetalerts2.php
+++ secode/views/templates/sweetalerts2.php
@@ -4,5 +4,3 @@
 <!-- use Sweet Alerts2 -->
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-<script src="sweetalert2.min.js"></script>
-<link rel="stylesheet" href="sweetalert2.min.css">
