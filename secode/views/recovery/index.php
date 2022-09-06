d680a668e24a9c63e54a08197bad2a9ff087577c
Luis Fernando <luis.chaparro173@educacionbogota.edu.co>
2022-09-05 23:33:06 -0500

    fix link password recovery

--- secode/views/recovery/index.php
+++ secode/views/recovery/index.php
@@ -23,8 +23,11 @@ if (isset($_POST['CorreoUser'])) {
                 !empty($results) &&
                 count($results) == 1
             ) {
-                $tokenReset=$results['Token_reset'];
-                echo $tokenReset;
+                $tokenReset = $results['Token_reset'];
+                echo "<a href='http://localhost/SECODE_QR/secode/views/recovery/recupera.php?tokenUserMail=$tokenReset' target='BLANK'> RECUPERAR CONTRASEÑA</a>";
+
+                //Datos de envio smtp despliegue app.
+
                 /*
                 $template =  `
                 
@@ -82,38 +85,6 @@ if (isset($_POST['CorreoUser'])) {
     <center>
 
         <script>
-            var x = document.getElementById("frmlogin");
-            var y = document.getElementById("frmregistrar");
-            var z = document.getElementById("btnvai");
-            var textcolor1 = document.getElementById("vaibtnlogin");
-            var textcolor2 = document.getElementById("vaibtnregistrar");
-            textcolor1.style.color = "white";
-            textcolor2.style.color = "black";
-
-            /*function registrarvai() {
-
-                x.style.left = "-400px";
-                y.style.left = "50px";
-                z.style.left = "110px";
-                textcolor1.style.color = "black";
-                textcolor2.style.color = "white";
-
-            }*/
-            function loginvai() {
-
-                x.style.left = "50px";
-                y.style.left = "450px";
-                z.style.left = "0";
-                textcolor1.style.color = "white";
-                textcolor2.style.color = "black";
-
-            }
-
-            function abrirform() {
-                document.getElementById("formrecupera").style.display = "block";
-
-            }
-
             function cancelarform() {
                 document.getElementById("formrecupera").style.display = "none";
             }
